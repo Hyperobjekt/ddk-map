9e164d36eff0c3fe3fa44b5a13a81071b5281499
Josh Maas-Howard <j.maas.howard@gmail.com>
2020-12-03 18:31:14 -0500

    SHARING: implement desktop unified share btn with popover for share options

--- src/constants/lang.js
+++ src/constants/lang.js
@@ -4,5 +4,14 @@ export const langSet = {
     MAP_LOADING_DATA: `Loading map data`,
     MAP_UI_POWERED_BY: `Custom Attribution ©`,
     MAP_TEST_MERGE_EXTRA: `Test`,
+    SHARE: 'Share',
+    BUTTON_SHARE_TWITTER: 'Share on Twitter',
+    DIALOG_SHARE_TWITTER: 'share text', // TODO
+    BUTTON_SHARE_FACEBOOK: 'Share on Facebook',
+    DIALOG_SHARE_FACEBOOK: 'share text', // TODO
+    BUTTON_SHARE_EMAIL: 'Share via email',
+    DIALOG_SHARE_EMAIL: 'share text', // TODO
+    BUTTON_SHARE_LINK: 'Get a custom link',
+    DIALOG_SHARE_LINK: 'share text', // TODO
   },
 }
