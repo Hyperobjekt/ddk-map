ce4d640aa23c3c10c4d45a03abdba853500db401
Lou Groshek <lou@lougroshek.com>
2021-02-26 18:47:41 -0600

    Adds fly-to buttons

---
+++ src/modules/ddk/explorer/Map/components/FlyToResetBtn.js
@@ -0,0 +1,46 @@
+import React, { useState } from 'react'
+import PropTypes from 'prop-types'
+import clsx from 'clsx'
+import i18n from '@pureartisan/simple-i18n'
+import { makeStyles } from '@material-ui/core/styles'
+import { Tooltip, Button } from '@material-ui/core'
+import shallow from 'zustand/shallow'
+
+import useStore from './../../store'
+import { DEFAULT_VIEWPORT } from './../../../../../constants/map'
+
+const FlyToResetBtn = ({ children, ...props }) => {
+  const { flyToLatLon } = useStore(
+    state => ({
+      flyToLatLon: state.flyToLatLon,
+    }),
+    shallow,
+  )
+
+  const handleClick = () => {
+    // flyToReset()
+    flyToLatLon(
+      DEFAULT_VIEWPORT.latitude,
+      DEFAULT_VIEWPORT.longitude,
+      DEFAULT_VIEWPORT.zoom,
+    )
+  }
+
+  return (
+    <Tooltip title={i18n.translate(`MAP_RESET`)} arrow>
+      <Button
+        aria-label={i18n.translate(`MAP_RESET`)}
+        onClick={handleClick}
+        placement={props.placement}
+      >
+        {children}
+      </Button>
+    </Tooltip>
+  )
+}
+
+FlyToResetBtn.propTypes = {
+  placement: PropTypes.string,
+}
+
+export default FlyToResetBtn
